2ce4475252cc10379523fc060d627bb1e0db76b6
nawm77 <alpha050603@gmail.com>
2023-12-16 22:33:43 +0300

    add comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from service import performance_service
 msg = "Введите номер действия для продолжения работы"
 
 
+# Отображаем первоначальное меню ввода для пользователя
 def menu():
     print("Menu:")
     print("1) меню действия с актерами")
@@ -15,6 +16,7 @@ def menu():
     print("5) Завершение работы")
 
 
+# Отображаем меню для работы с конкретной сущностью
 def entity_menu(entity):
     print(f"1) Получить все объекты сущности {entity}")
     print(f"2) Получить объект сущности {entity} по его ID")
@@ -23,6 +25,7 @@ def entity_menu(entity):
     print(f"5) Редактировать объект сущности {entity} по его ID")
 
 
+# Валидируем то, что написал нам пользователь (цифра это, или строка)
 def validate_user_input(message, type):
     while True:
         print(message)
@@ -35,6 +38,9 @@ def validate_user_input(message, type):
             print("Неверный формат входжных данных. Введите повторно корректные данные")
 
 
+# возвращаем для работы определенный сервис, для того, чтобы избежать проблему дублирования одного и того же кода
+# В лучшем случае можно вообще выдавать ошибку, что нет корректного сервиса,
+# но в нашем случае это не нужно, тк мы сами эти сервисы в коде вызываем, а не пользователь вызывает
 def on_action_with_entity(entity):
     if entity == "actor":
         return actor_service
@@ -47,6 +53,7 @@ def on_action_with_entity(entity):
     return None
 
 
+# Производим одно из действий, которое у всех одинаковое (получение всех, получение одного, судаление одного)
 def do_action(action_service, action):
     if action == 1:
         print(action_service.get_all())
@@ -58,9 +65,10 @@ def do_action(action_service, action):
         print(action_service.delete_one_by_id(id))
     print("--------------------------")
 
-
+# main метод для запуска программы
 def main():
     while True:
+        # отображаем меню с первоначальным вводом
         menu()
         user_choice = validate_user_input(msg, "number")
         if user_choice == 5:
@@ -68,9 +76,13 @@ def main():
             break
         #     Для актера
         elif user_choice == 1:
+            # подменяем реализацию сервиса
             current_service = on_action_with_entity("actor")
+            # отображаем меню для действий к актеру
             entity_menu("actor")
+            # определяем экшн (дейтсиве), которое будем делать (создание, удаление, поиск, обновление)
             action = validate_user_input(msg, "number")
+            # если же мы работаем с созданием, или обновлением, то пока что можно написать так, тк у всех сущностей разное кол-во полей
             if action == 4:
                 name = validate_user_input("Введите имя актера", "string")
                 age = validate_user_input("Введите возраст актера", "number")
@@ -86,6 +98,7 @@ def main():
                 current_service.update_one_by_id(id, actor)
             else:
                 do_action(current_service, action)
+        #         TODO доделать реализацию для всех сущностей + сделать вывод итога для создания, удаления и обновления сущности, как делали это в конце занятия в сервисе. То есть печатать удалось ли сделать или нет
         elif user_choice == 2:
             current_service = on_action_with_entity("audience")
             entity_menu("audience")
